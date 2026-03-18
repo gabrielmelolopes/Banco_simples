aedd3965a4172a29b1d41f2b345905bc3dc442f8
Gabriel Melo <gabrielmelolopes.br@gmail.com>
2026-03-17 21:19:07 -0300

    Create MenuString class with menu constants
    Added main and secondary menu strings for user interaction.

---
+++ MenuString.java
@@ -0,0 +1,19 @@
+public class MenuString {
+        public static final String[] mainMENU = {
+                        "========== MENU PRINCIPAL ==========",
+                        "1 - Fazer Cadastro",
+                        "2 - Fazer Login",
+                        "0 - Sair",
+                        "====================================",
+        };
+
+        public static final String[] secundaryMENU = {
+                        "=========================",
+                        "1 - Ver Saldo pelo nome",
+                        "2 - Depositar pelo nome",
+                        "3 - Sacar",
+                        "4 - Transferir",
+                        "0 - Logout",
+                        "=========================",
+        };
+}
